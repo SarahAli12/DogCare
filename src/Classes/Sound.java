770de55bd6c7a867e33f490d0f521a1a0c723786
Sarah Ali <36427657+SarahAli12@users.noreply.github.com>
2019-01-15 22:04:28 -0500

    Add files via upload

---
+++ src/Classes/Sound.java
@@ -0,0 +1,55 @@
+import javax.sound.sampled.*;
+import java.io.*;
+import java.net.*;
+
+public class Sound
+{
+    private Clip clip;
+
+    public Sound()
+    {
+
+    }
+
+    public Sound(String filename)
+    {       
+        File file = new File(filename);
+        try
+        {
+            if(file.exists())
+            {
+                AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+                clip = AudioSystem.getClip();
+                clip.open(sound);
+            }
+        }
+        catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play()
+    {
+        clip.setFramePosition(0);
+        clip.start();
+    }
+
+    public void loop()
+    {
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop()
+    {
+        clip.stop();
+    }
+}
